272edd2e5ed6f302fbdf38c815b3ea771909be09
Saul Valdez <saulvaldez472@gmail.com>
2022-04-18 19:23:26 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PruebaTecnica_consumo_de_API_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linea,url,json;
+            int desicion;
+
+            Console.WriteLine("¿Desea realizar la busqueda por Id(1),Genero(2),Estado(3)?:");
+            linea = Console.ReadLine();
+            desicion = Int32.Parse(linea);
+
+            if (desicion==1)
+            {
+
+
+                if (desicion==2)
+                {
+                    if (desicion==3)
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+
+            }
+
+
+
+        }
+    }
+}
