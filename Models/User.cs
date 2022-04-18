e0aa5e25f51142d299981113dc9853b615437cf8
Saul Valdez <saulvaldez472@gmail.com>
2022-04-18 20:07:51 -0300

    Creacion del Objeto para la peticion

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_consumo_de_API_.Models
+{
+ public   class User
+    {
+
+         int Id { get; set; }
+
+        string Name { get; set; }
+        string Email { get; set; }
+        string Gender { get; set; }
+        string Status { get; set; }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PruebaTecnica_consumo_de_API_
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string linea,url,json;
+            string linea,url= "https://gorest.co.in/public/v2/users", json;
             int desicion;
 
-            Console.WriteLine("¿Desea realizar la busqueda por Id(1),Genero(2),Estado(3)?:");
+            HttpClient client = new HttpClient();
+            var responsehttp = await client.GetAsync(url);
+
+            Console.WriteLine("¿Desea realizar la busqueda por Id(1),Genero(2),Estado(3),Obtener todos los registros(cualquier caracter)?:");
             linea = Console.ReadLine();
             desicion = Int32.Parse(linea);
 
